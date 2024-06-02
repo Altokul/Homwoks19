abf4e4a111d6b95ae1102d23565314107b53eea5
Altokul <senkyou963@gmail.com>
2024-06-03 00:01:46 +0300

    Дякую за ваші зауваження! Використав метод з SecurityChecker::sanitize для фільтрації введених даних.
    Signed-off-by: Altokul <senkyou963@gmail.com>

--- homeworks/homework21/backend.php
+++ homeworks/homework21/backend.php
@@ -1,35 +1,48 @@
 <?php
 
-include 'CookieManager.php';
+require_once 'homeworks\homework21\system\SecurityChecker.php';
+require_once 'homeworks\homework21\system\CookieManager.php.php';
 
-$cookieManager = new CookieManager();
+
 $message = '';
 
 try {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (isset($_POST['set_cookie'])) {
-            $name = $_POST['name'] ?? '';
-            $value = $_POST['value'] ?? '';
+            $name = SecurityChecker::sanitize($_POST['name'] ?? '');
+            $value = SecurityChecker::sanitize($_POST['value'] ?? '');
             $expiry = intval($_POST['expiry'] ?? 0);
-            $cookieManager->setCookie($name, $value, $expiry);
+
+            if (!SecurityChecker::checkStringLength($name, 1, 255) || !SecurityChecker::checkStringLength($value, 1, 255)) {
+                throw new Exception("Ім'я та значення кукі мають бути від 1 до 255 символів.");
+            }
+
+            CookieManager::setCookie($name, $value, $expiry);
             $message = "Cookie '{$name}' створено.";
+
         } elseif (isset($_POST['delete_cookie'])) {
-            $name = $_POST['name'] ?? '';
-            $cookieManager->deleteCookie($name);
+            $name = SecurityChecker::sanitize($_POST['name'] ?? '');
+
+            CookieManager::deleteCookie($name);
             $message = "Cookie '{$name}' видалено.";
+
         } elseif (isset($_POST['verify_cookie'])) {
-            $name = $_POST['name'] ?? '';
-            $value = $_POST['value'] ?? '';
-            $message = $cookieManager->verifyCookieValue($name, $value) ? "Значення кукі '{$name}' підтверджено." : "Значення кукі '{$name}' не відповідає.";
+            $name = SecurityChecker::sanitize($_POST['name'] ?? '');
+            $value = SecurityChecker::sanitize($_POST['value'] ?? '');
+            $message = CookieManager::verifyCookieValue($name, $value) ? "Значення кукі '{$name}' підтверджено." : "Значення кукі '{$name}' не відповідає.";
+
         } elseif (isset($_POST['get_cookie'])) {
-            $name = $_POST['name'] ?? '';
-            $cookieValue = $cookieManager->getCookie($name);
+            $name = SecurityChecker::sanitize($_POST['name'] ?? '');
+
+            $cookieValue = CookieManager::getCookie($name);
             $message = $cookieValue !== null ? "Значення кукі '{$name}': $cookieValue" : "Кукі '{$name}' не існує.";
         }
     }
 } catch (Exception $e) {
     $message = 'Помилка: ' . $e->getMessage();
 }
+
+
 
 
 
