fce4e6e2df9f0a42e6bbfde072f2a4a894247a83
Altokul <senkyou963@gmail.com>
2024-06-03 00:18:02 +0300

    Дякую за ваші зауваження! Добавив типізацію Переробив метод sanitize,тепер він видаляя пробіли з початку та кінця рядка,та конвертує спец.символи що запобігає XSS-атакам,майже
    Signed-off-by: Altokul <senkyou963@gmail.com>

--- homeworks/homework21/system/SecurityChecker.php
+++ homeworks/homework21/system/SecurityChecker.php
@@ -5,19 +5,19 @@ namespace homeworks\homework21\system;
 class SecurityChecker
 {
 
-    public static function sanitize($data)
+    public static function sanitize(string $data): string
     {
-        return htmlspecialchars(trim($data));
+        return htmlspecialchars(trim($data), ENT_QUOTES, 'UTF-8');
     }
 
-    public static function checkStringLength($str, $minLength, $maxLength)
+    public static function checkStringLength(string $str, int $minLength, int $maxLength): bool
     {
         $len = mb_strlen($str, 'UTF-8');
         return $len >= $minLength && $len <= $maxLength;
     }
 
-    public static function hasSpecialCharacters($str)
+    public static function hasSpecialCharacters(string $str): bool
     {
-        return preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $str);
+        return (bool)preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $str);
     }
 }
